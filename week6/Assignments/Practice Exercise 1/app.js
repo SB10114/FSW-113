d6aa6761cc90cf03e3dbdeea4e7bcfd716e98412
Sierra <sierra.butler@bryanuniversity.edu>
2021-08-19 12:55:12 -0500

    submit

---
+++ week6/Assignments/Practice Exercise 1/app.js
@@ -0,0 +1,30 @@
+// var printData = function(data){
+//     return function () {
+//         var sentence = data.toLowerCase().split(" ")
+//         for(i = 0; i< sentence.length; i++){
+//             sentence[i] = sentence[i][0].toUpperCase() + sentence[i].slice(1)
+//         }
+//         return sentence.join("")
+//     }
+// }
+
+// const print = printData
+// const output = print("Hi there, how are you")
+// const result = output()
+// console.log(result)
+
+// const myArrays = [4, 5, 6, 7, 8]
+// const newArray = myArrays.map(item => item * 2)
+// console.log(newArray)
+// console.log(newArray)
+
+// const myArrays = [4, 5, 6, 7, 8]
+// const newArray = Object.assign({}, myArrays, {count: myArrays.length})
+// console.log(myArrays)
+// console.log(newArray)
+
+// const myArrays = [4, 5, 6, 7, 8]
+// const newArray = {...myArrays, count: myArrays.length}
+// console.log(myArrays)
+// console.log(newArray)
+
